7abb9a953d353630d323f09d1698a981c2b938e5
gokhanaytekinn <gokhan.746@gmail.com>
2026-02-21 15:09:03 +0300

    feat: Add initial onboarding screen, app icons, and project build configuration.

--- app/src/main/java/com/gokhanaytekinn/sdandroid/ui/screens/OnboardingScreen.kt
+++ app/src/main/java/com/gokhanaytekinn/sdandroid/ui/screens/OnboardingScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
