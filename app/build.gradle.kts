61432f83cf319b853010501792fd9cff7d935785
gokhanaytekinn <gokhan.746@gmail.com>
2026-03-01 16:03:43 +0300

    feat: Add initial `build.gradle.kts` for the Android application, configuring build settings, plugins, and dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.gokhanaytekinn.sdandroid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
