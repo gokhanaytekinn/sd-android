09d8551518d1c0a02c74162c0a5085a44d7c8469
gokhanaytekinn <gokhan.746@gmail.com>
2026-03-02 23:15:43 +0300

    build: Add google-services.json for Firebase and Google services configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.gokhanaytekinn.sdandroid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
